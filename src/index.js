653e92b157e8e59ccd0f484551df19b56fc90450
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2023-11-28 21:04:35 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// 2nd approach for database connection- this is better than 1st approach 
+
+// require('dotenv).config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"; 
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+// 1st approach for database connection
+
+/* import mongoose from "mongoose";
+    import { DB_NAME } from "./constants";
+
+// i think you know iffe in js 
+
+import express from "express";
+const app = express()
+
+( async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error) => {
+        console.log("ERROR:", error );
+        throw error;
+    })
+
+    app.listen(process.env.PORT, () =>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("ERROR:", error)
+    throw error
+  }
+}) () */
