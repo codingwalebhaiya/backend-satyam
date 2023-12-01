804e0518aa3a343fb7fd9c9d5d1198b836a60a1e
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2023-12-02 01:22:20 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 // require('dotenv).config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"; 
+import { app } from "./app.js";
 
 dotenv.config({
   path: './env'
-})
+}) 
 
 connectDB()
 .then(() => {
@@ -15,7 +16,7 @@ connectDB()
    throw error
   })
   app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running  at port: ${process.env.PORT}`);
+    console.log(`Server is running  at port: ${process.env.PORT}`); 
   }) 
 })
 .catch((err) => {
