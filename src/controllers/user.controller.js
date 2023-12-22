c7143d29ecc11f495e12fcadfc771a98cfb9d55d
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2023-12-23 02:18:07 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
   //01- // get user details from frontend 
    const {fullName, email, username, password} = req.body 
-   console.log("email", email); 
+   // console.log("email", email); 
 
    //02- validation - not empty 
   if (
@@ -28,16 +28,25 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new ApiError(400, "All fields are required")
   }
 
-  const existedUser = User.findOne({
-    $or: [{username}, {email}]
+  const existedUser = await User.findOne({
+    $or: [{username}, {email}] 
   })
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists")
   }
 
+  //console.log(req.files); 
+
+  // for avatar 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // for coverImage
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required")
@@ -63,18 +72,16 @@ const registerUser = asyncHandler( async (req, res) => {
   )
 
   const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken" 
   )
 
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user")
-  }
+  } 
 
   return res.status(201).json(
     new ApiResponse(200, createdUser, "User registered Successfully")
-  )
-
-
+  ) 
 
 } )
 
